4b046cdd0090c089da05213133e71a058c7f183c
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-26 11:35:33 +0200

    first glance on utilities

---
+++ pages/utilities.js
@@ -0,0 +1,9 @@
+function Utilities(){
+    return(
+        <>
+            <button className=" bg-sky-300 border-2 text-white font-bold hover:bg-sky-900 rounded py-2 px-4">Save Changes</button>
+        </>
+    )
+}
+
+export default Utilities;
