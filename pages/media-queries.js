abce7733ed6b759085980cb7d428398c6fd49cf5
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-03 11:56:30 +0200

    taste on breakpoints

---
+++ pages/media-queries.js
@@ -0,0 +1,11 @@
+function MediaQueries() {
+    return (
+        <div className="flex flex-col items-center">
+            <div className=" sm:bg-green-600 md:bg-yellow-400 lg:bg-purple-600 xl:bg-red-500 text-white mt-6 bg-blue-500">
+                <p>Welcome to Pseudo Elements</p>
+            </div>
+        </div>
+    )
+}
+
+export default MediaQueries;
