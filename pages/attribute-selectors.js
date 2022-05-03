2eb029f77106f687310c3d9e208d78fed6d1f3a8
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-03 12:54:32 +0200

    a little test on rtl and ltr

--- pages/attribute-selectors.js
+++ pages/attribute-selectors.js
@@ -1,7 +1,9 @@
 function AttributSelector(){
     return (
-        <div>
-            
+        <div className="flex flex-col px-6 py-4">
+            <h1 className=" mb-2 text-center">RTL & LTR</h1>
+            <p className=" ltr:mr-3" dir="ltr"> Welcome to left to right</p>
+            <p className=" rtl:ml-3" dir="rtl"> Welcome to right to left</p>
         </div>
     )
 }
