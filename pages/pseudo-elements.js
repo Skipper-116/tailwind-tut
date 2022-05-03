23a592a3ae6899dad87668c0eb43ed895b1c5fb5
Roy Chanunkha <roychanunkha@gmail.com>
2022-05-03 11:07:19 +0200

    learnt how to use first-letter utility

--- pages/pseudo-elements.js
+++ pages/pseudo-elements.js
@@ -19,7 +19,7 @@ function PseudoElements() {
                 <input type="file" className=" block w-full text-sm text-slate-400 file:mr-4 file:py-2 file:px-4 file:rounded-full file:bg-pink-400 file:text-pink-700 hover:file:bg-pink-100" />
             </label>
             <div className="mt-9 selection:bg-fuchsia-900 selection:text-white">
-                <p>
+                <p className="first-letter:float-left first-letter:text-8xl first-letter:font-bold">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
